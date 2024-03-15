cd4607d0842e642d0acc13551290f03a29454a9f
André Borges <andreborgescastro@gmail.com>
2024-03-15 17:58:58 -0300

    Adiciona uma biblioteca de cryptografiapara simular um ambiente de autenticação. E remove variáveis de ambiente desnecessárias.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -6,12 +7,33 @@ export class AuthService {
   async signIn(username: string, pass: string): Promise<any> {
     /* Simulação de uma operação de login onde é salvo no banco de
      dados a enha criptografada com salt*/
-
-    const user = { username: 'usuario01', password: 'senha01' };
-    if (user?.password !== pass) {
+    const user = {
+      username: 'usuario01',
+      password: '$2a$10$zZy8RiqVMBtwg2LAMRmvM.wWc9n/0DyYWuQU7F4lxdmM3QOR3iyTS',
+    };
+    const isAuthorized = await this.compareEncrypted(pass, user.password);
+    if (!isAuthorized) {
       throw new UnauthorizedException();
     }
     const { password, ...result } = user;
     return result;
+  }
+
+  /* Função que representaria um módulo específico para criptografia.
+   * levando em consideração a redução de complexidade
+   * resolvi condensar a operação em uma única função.
+   */
+
+  async encrypt(value: string): Promise<string> {
+    const rounds = 10;
+    const salt = await bcrypt.genSalt(rounds);
+    return await bcrypt.hash(value, salt);
+  }
+
+  async compareEncrypted(
+    value: string,
+    encryptedValue: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(value, encryptedValue);
   }
 }
