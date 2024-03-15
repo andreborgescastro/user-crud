4b764ce1e9c17a424edb6f462dc8bb730effb8cf
André Borges <andreborgescastro@gmail.com>
2024-03-15 10:21:55 -0300

    Ajusta os campos da migration de criação da tabela de usuario.

--- infra/migrations/01.cria_tabela_usuario.js
+++ infra/migrations/01.cria_tabela_usuario.js
@@ -6,22 +6,23 @@ module.exports = {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
-        autoIncrement: true,
       },
       cpf: {
         type: Sequelize.STRING(255),
         unique: true,
+        allowNull: false,
       },
-      nome: Sequelize.STRING(255),
-      data_nascimento: Sequelize.DATE,
+      nome: { type: Sequelize.STRING(255), allowNull: false },
+      data_nascimento: { type: Sequelize.DATE, allowNull: false },
       status: {
         type: Sequelize.BOOLEAN,
         defaultValue: true,
+        allowNull: false,
       },
-      criado_em: Sequelize.DATE,
-      usuario_criacao: Sequelize.STRING(255),
-      removido_em: Sequelize.DATE,
-      usuario_remocao: Sequelize.STRING(255),
+      criado_em: { type: Sequelize.DATE, allowNull: false },
+      usuario_criacao: { type: Sequelize.STRING(255), allowNull: false },
+      removido_em: { type: Sequelize.DATE },
+      usuario_remocao: { type: Sequelize.STRING(255) },
     });
   },
 
